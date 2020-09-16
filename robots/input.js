2184fd70017ff1e6f18779ca3e247093f08831d8
Filipe <filipe@Filipe-MacBook.local>
2020-09-16 00:42:00 -0300

    feat: add input robot and state save

---
+++ robots/input.js
@@ -0,0 +1,28 @@
+const readline = require('readline-sync')
+const state    = require('./state.js')
+
+function robot (){
+    const content = {
+        maximumSentences: 8
+    }
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+    state.save(content)
+
+    function askAndReturnSearchTerm(){
+        return readline.question('Digite um termo da Wikipedia')
+
+    }
+
+    function askAndReturnPrefix(){
+        const prefixs = ['Quem e ', 'O que é', 'A Historia de']
+        const selectedPrefixIndex = readline.keyInSelect(prefixs, 'Escolha uma opção')
+        const selectedPrefixText  = prefixs[selectedPrefixIndex]
+     
+        return selectedPrefixText
+    }
+
+}
+
+module.exports = robot
